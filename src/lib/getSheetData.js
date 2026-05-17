157bcdbff3f730a1621541a95918c1f3478cd815
北川侑可 <kitagawayuka@Yuka-NEWs-MacBook-Pro-M3.local>
2026-05-17 12:14:03 +0900

    “260517update”

--- src/lib/getSheetData.js
+++ src/lib/getSheetData.js
@@ -5,12 +5,30 @@ export async function getSheetData(){
   const json = JSON.parse(text.substring(47).slice(0, -2));
 
   return json.table.rows.map((row, index) => {
+    const rawDate = row.c[0]?.f || "";
+    const date = rawDate.slice(0, 16);
+
+    const month = rawDate.slice(0, 7);
+
     return {
       slug: `diary${index + 1}`,
-      date: (row.c[0]?.f || "").slice(0, 16),
+      date,
+      month,
       title: row.c[1]?.v,
       body: row.c[2]?.v,
       emoji: row.c[3]?.v,
     };
   });
 }
+
+const data = await getSheetData();
+
+const groupedData = {};
+
+data.forEach(entry => {
+  if (!groupedData[entry.month]) {
+    groupedData[entry.month] = [];
+  }
+
+  groupedData[entry.month].push(entry);
+});
